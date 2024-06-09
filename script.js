85e913856ff8dbd4072c6972be2b991f24e4dcfa
hakizaoli <148442654+hakizaoli@users.noreply.github.com>
2024-06-09 21:56:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+imgCard = document.querySelectorAll(".img-card");
+
+imgCard.forEach(element => {
+
+  element.addEventListener("click", function(e){
+
+    imgCard.forEach(el =>{
+      el.classList.remove("active");
+    });
+    element.classList.add("active");
+  })
+
+});
